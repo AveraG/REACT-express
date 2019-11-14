b29ba16168aee7c28ca1a608954d21b630d0c723
Avera Guthrie <avera@Averas-MacBook-Pro.local>
2019-11-14 16:59:54 -0600

    React start, backend works

--- express-backend/index.js
+++ express-backend/index.js
@@ -3,20 +3,20 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
+const playersRouter = require("./routes/players");
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.use(logger('dev'));
 app.use(cors());
 
-app.use(bodyParser.erlencoded({ estended:true }));
-app.use(bodyParser.json());
+app.use(express.json());
 
-const playersRouter = require("./routes/players");
+
 app.use("/players", playersRouter);
 
 app.listen(port, () => {
-    console.log('running on' + port);
+    console.log('running on port ' + port);
 });
 
 module.exports = app;
